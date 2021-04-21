59b3c935b56398ef97ec877279d3a417069a21b8
TomAlfano <tomalfanohughes@gmail.com>
2021-04-21 22:52:43 +0100

    Form redirects! (maybe works completely). Had missed 1 line of code in the html, fml

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from .forms import  InputForm
 
 import requests
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f5f35514a2e828fe0c328b21391b80ec273fcf680c98b1f3'
+app.config['SECRET_KEY'] = 'ShhItsASecret'
 
 
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
@@ -24,12 +24,21 @@ def search_page():
     form = InputForm()
     if form.validate_on_submit():
         ingredientInput = {'meat':numberize(form.meat.data),'fish':numberize(form.fish.data),'vegetable':numberize(form.vegetable.data),'fruit':numberize(form.fruit.data),'pasta':numberize(form.pasta.data),'rice':numberize(form.rice.data),'egg':numberize(form.egg.data),'dairy':numberize(form.dairy.data),'pizza':numberize(form.pizza.data)}
-        return redirect(url_for('results'))
+        return redirect(url_for('result'))#, ingredientInput=ingredientInput))
     return render_template('website.html', form=form)
 
-@app.route("/results")
-def results():
-    return render_template('results.html')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    form = InputForm()
+    if form.validate_on_submit():
+        ingredientInput = {'meat':numberize(form.meat.data),'fish':numberize(form.fish.data),'vegetable':numberize(form.vegetable.data),'fruit':numberize(form.fruit.data),'pasta':numberize(form.pasta.data),'rice':numberize(form.rice.data),'egg':numberize(form.egg.data),'dairy':numberize(form.dairy.data),'pizza':numberize(form.pizza.data)}
+        return redirect(url_for('result'))#, ingredientInput=ingredientInput))
+    return render_template('website.html', form=form)
+
+@app.route("/result/")
+def result():
+
+    return render_template('result.html', title="Results")
 
 if __name__ == '__main__':
   app.run()
