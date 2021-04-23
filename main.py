6da5828924ba00631bd05c1e39328c563f6df9e7
TomAlfano <tomalfanohughes@gmail.com>
2021-04-23 12:59:24 +0100

    removed duplicate code from main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, request, url_for, redirect
-from .forms import  InputForm
-from flask_sqlalchemy import SQLAlchemy
+from .forms import  InputForm, locations
 
 import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ShhItsASecret'
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://c1843151:Canada3424@csmysql.cs.cf.ac.uk:3306/c1843151_flask_blog'
-db = SQLAlchemy(app)
 
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 
@@ -15,41 +12,27 @@ headers = {
   'x-rapidapi-key': "<YOUR_RAPID_API_KEY>",
   }
 
+#sets global variable for SQL work-around
 global ingredientInput
-ingredientInput = "please"
+ingredientInput = 'Please click "Find Recipe"'
 
+#changes checkbox input from True/False to 1/0
 def numberize(checkBoxInput):
     if checkBoxInput == False:
         return 0
     else:
         return 1
 
+#landing page
 @app.route('/', methods=['GET', 'POST'])
 def search_page():
     form = InputForm()
+    #message = 'Please click "Find Recipe"'
     if form.validate_on_submit():
         global ingredientInput
-        ingredientInput = {'meat':numberize(form.meat.data),'fish':numberize(form.fish.data),'vegetable':numberize(form.vegetable.data),'fruit':numberize(form.fruit.data),'pasta':numberize(form.pasta.data),'rice':numberize(form.rice.data),'egg':numberize(form.egg.data),'dairy':numberize(form.dairy.data),'pizza':numberize(form.pizza.data)}
-        return redirect(url_for('result', value=ingredientInput, form=form))
-    return render_template('website.html', form=form)
-
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    form = InputForm()
-    if form.validate_on_submit():
-        global ingredientInput
-        ingredientInput = {'meat':numberize(form.meat.data),'fish':numberize(form.fish.data),'vegetable':numberize(form.vegetable.data),'fruit':numberize(form.fruit.data),'pasta':numberize(form.pasta.data),'rice':numberize(form.rice.data),'egg':numberize(form.egg.data),'dairy':numberize(form.dairy.data),'pizza':numberize(form.pizza.data)}
-        return redirect(url_for('result', value=ingredientInput, form=form))
-    return render_template('website.html', form=form)
-
-@app.route("/result/", methods=['GET', 'POST'])
-def result():
-    form = InputForm()
-    if form.validate_on_submit():
-        global ingredientInput
-        ingredientInput = {'meat':numberize(form.meat.data),'fish':numberize(form.fish.data),'vegetable':numberize(form.vegetable.data),'fruit':numberize(form.fruit.data),'pasta':numberize(form.pasta.data),'rice':numberize(form.rice.data),'egg':numberize(form.egg.data),'dairy':numberize(form.dairy.data),'pizza':numberize(form.pizza.data)}
-        return redirect(url_for('result', value=ingredientInput, form=form))
-    return render_template('result.html', title="Results", form=form, value = ingredientInput)
+        ingredientInput = {'country':dict(locations).get(form.location.data),'meat':numberize(form.meat.data),'fish':numberize(form.fish.data),'vegetable':numberize(form.vegetable.data),'fruit':numberize(form.fruit.data),'pasta':numberize(form.pasta.data),'rice':numberize(form.rice.data),'egg':numberize(form.egg.data),'dairy':numberize(form.dairy.data),'pizza':numberize(form.pizza.data)}
+        return redirect(url_for('search_page', value=ingredientInput, form=form))
+    return render_template('website.html', title="Popty" , form=form, value=ingredientInput)
 
 if __name__ == '__main__':
   app.run()
